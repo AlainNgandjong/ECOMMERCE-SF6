84d6e4d19253073755ad2cfd8552b2e31d52a01b
AlainNgandjong <a.ngandjong@gmail>
2022-03-04 15:28:17 +0100

    Add style in login and register template

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,25 +4,75 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('lastname')
-            ->add('firstname')
-            ->add('address')
-            ->add('zipcode')
-            ->add('city')
+            ->add('email', EmailType::class, [
+                'label' => '@',
+                'attr' => [
+                    'placeholder' => 'Lastname',
+                ],
+                'row_attr' => [
+                    'class' => 'input-group',
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Lastname',
+                'attr' => [
+                    'placeholder' => 'Lastname',
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ],
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Firstname',
+                'attr' => [
+                    'placeholder' => 'Firstname',
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ],
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Address',
+                'attr' => [
+                    'placeholder' => 'Address',
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ],
+            ])
+            ->add('zipcode', TextType::class, [
+                'label' => 'Zipcode',
+                'attr' => [
+                    'placeholder' => 'Zipcode',
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ],
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'City',
+                'attr' => [
+                    'placeholder' => 'City',
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -30,12 +80,21 @@ class RegistrationFormType extends AbstractType
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
+                'label_attr' => [
+                    'class' => 'checkbox-inline checkbox-switch',
+                ],
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'placeholder' => 'Password',
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
